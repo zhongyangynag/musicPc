e4d15a635ce719a22352d6fbe53fd78be3355797
zhongyang <zhongyang@carsvs.com>
2020-05-27 16:28:10 +0800

    。。。

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+require('video.js/dist/video-js.css')
+require('vue-video-player/src/custom-theme.css')
+// require('…/static/css/myvideo.css') //如果你需要自定义播放器的样式，自己新建一个css
+import VideoPlayer from 'vue-video-player'
+//引入 hls
+import 'videojs-contrib-hls'
+Vue.use(VideoPlayer)
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
 Vue.use(VueRouter)
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
   },
   {
     path: '/about',
