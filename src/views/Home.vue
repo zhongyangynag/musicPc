677c0313b1cb0aefa8cbec2d4e428468f7432afb
zhongyangyang <zhongyang@zhongyangyang.com>
2020-12-27 17:47:43 +0800

    测试1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@
         <!--        歌单-->
         <div style="margin-top: 10px;margin-bottom: 10px">
             <div class="mType">
-                <div class="mTypeText">精选233</div>
+                <div class="mTypeText">精选</div>
                 <div class="mTypeChose">
                     <span :class="[this.listType==='hot'?'hot':'']" @click="playlist('hot')">最热</span>
                     <span class="line"></span>
