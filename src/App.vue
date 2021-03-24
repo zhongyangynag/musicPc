2836a5274ccbe5ce72611fe3530a7c4d1599c206
liamzhong <liamzhong@tencent.com>
2021-03-24 16:53:23 +0800

    登录删除默认信息

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
     <div id="app">
         <Header></Header>
         <div style="width: 100%;height: 40px"></div>
-
         <router-view/>
         <div v-if="showPlay" class="play">
             <Play/>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -245,7 +245,7 @@
         background-repeat: no-repeat;
         background-position: center;
         background-size: cover;
-        filter: blur(10px);
+        filter: blur(50px);
     }
 
     .elImg {
